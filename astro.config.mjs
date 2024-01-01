67c440202836fac6da2bfb1be21d46153967f7df
Giuseppe Cutuli <cutuligiuseppe95@gmail.com>
2024-01-01 12:23:59 +0100

    feat: improved code, refactorized, starting portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import lottie from "astro-integration-lottie";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        lottie(),
+    ]
+});
