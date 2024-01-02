3e21a6d3b4e461d58536e4295c3bf415d80e4c73
Giuseppe Cutuli <cutuligiuseppe95@gmail.com>
2024-01-02 12:47:58 +0100

    feat: sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import lottie from "astro-integration-lottie";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        lottie(),
-    ]
+  site: 'https://giuseppe.cutuli.dev',
+  integrations: [lottie(), sitemap(), robotsTxt()]
 });
